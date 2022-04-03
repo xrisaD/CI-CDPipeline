7681841ca26ac08154ca13262f1ff792e9dbd863
Chrysa Dikonimaki <xrdikonimaki@hotmail.com>
2022-04-03 19:09:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({version: "0.0.0"})
+    res.json({version: "0.0.6"})
 })
 
 module.exports = app;
