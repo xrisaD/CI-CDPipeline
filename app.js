a9470bf65f9b05a9ea557b1736c5c37e5ab8e89c
xrisaD <xrdikonimaki@hotmail.com>
2022-04-05 00:54:59 +0200

    new change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({version: "0.x.3"})
+    res.json({version: "0.0.2"})
 })
 
 module.exports = app;
