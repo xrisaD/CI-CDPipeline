b3dc450d06d1217e247a2ae0fb689813b69f9bbf
Nick Smyrnioudis <smyrnioudis.nikolaos@outlook.com>
2022-04-04 19:11:00 +0200

    faulty version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({version: "0.2.3"})
+    res.json({version: "0.2.x"})
 })
 
 module.exports = app;
